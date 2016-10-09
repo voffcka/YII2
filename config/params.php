864b4c114fd2cd03333cb328c8642b856a636c59
voffcka <dj-boroda1@yandex.ru>
2016-10-09 13:50:15 +0300

    new file:                   .gitignore 	new file:                   config/.gitignore

--- config/params.php
+++ config/params.php
@@ -2,8 +2,8 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'urlAPI'=>'',
-    'subAPI'=>'',
-    'usrAPI'=>'',
-    'keyAPI'=>''
+    'urlAPI'=>'.amocrm.ru/private/api/',
+    'subAPI'=>'new57f77bacd86cf',
+    'usrAPI'=>'maill4vlad@yahoo.com',
+    'keyAPI'=>'99e12ee4cfa38c22303f0073c030df39'
 ];
